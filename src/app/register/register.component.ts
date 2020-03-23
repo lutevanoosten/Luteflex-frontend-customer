b2c36b7ed0f6e0e698456adbbd3f0a6f386b08c2
LuteIsCool <l.vanoosten@student.fontys.nl>
2020-03-23 23:33:02 +0100

    selecteren van pakket

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,4 +12,19 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  packageselected(pack: string): void {
+    let j;
+
+    const deselect = document.getElementsByClassName('selected');
+    while (deselect.length) {
+      deselect[0].classList.remove('selected');
+    }
+
+    const select = document.getElementsByClassName(pack);
+    for (j = 0; j < select.length; j++) {
+      select[j].classList.add('selected');
+    }
+
+  }
+
 }
