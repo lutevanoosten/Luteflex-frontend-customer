86b994602928d060ba0a406074360cf7a8e7c9df
LuteIsCool <l.vanoosten@student.fontys.nl>
2020-03-27 16:34:00 +0100

    watch pagina aangemaakt

--- src/app/movieterminal/movieterminal.component.ts
+++ src/app/movieterminal/movieterminal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {style} from '@angular/animations';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-movieterminal',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movieterminal.component.scss']
 })
 export class MovieterminalComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  openNav(): void {
+    document.getElementById('mySidenav').style.width = '275px';
+    document.getElementById('main').style.marginLeft = '275px';
+    document.getElementById('blur').style.width = '100%';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
+
+  closeNav(): void {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+    document.getElementById('blur').style.width = '0';
+    document.body.style.backgroundColor = 'white';
+  }
+
+  btnRight(): void {
+    document.getElementById('container').scrollTo({ left: (document.getElementById('container').scrollLeft + 400), behavior: 'smooth' });
+  }
+  btnLeft(): void {
+    document.getElementById('container').scrollTo({ left: (document.getElementById('container').scrollLeft - 400), behavior: 'smooth' });
+  }
 }
